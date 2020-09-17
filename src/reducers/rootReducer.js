a33055b036606327fc6fc9f1123738603be726d9
sonmin <sonbv@edmicro.vn>
2020-09-17 09:56:27 +0700

    remove some package no use

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-import formReducer from './formReducer';
 import homeworkTeacherReducer from './homeworkTeacherReducer';
 import PaperReducer from './paperReducer';
 import ReportReucer from './reportReducer';
@@ -8,7 +7,6 @@ import ReportReucer from './reportReducer';
 export default combineReducers({
   paper: PaperReducer,
   user: userReducer,
-  form: formReducer,
   homeworkTeacher: homeworkTeacherReducer,
   report:ReportReucer,
 });
