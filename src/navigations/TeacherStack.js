90d997cc1588502e2ed626171d0bcc12f15ceb74
Edmicro#3 <edmicro3@Mac-mini-cua-Edmicro3.local>
2020-11-23 11:56:32 +0700

    add Mission Statistics

--- src/navigations/TeacherStack.js
+++ src/navigations/TeacherStack.js
@@ -25,7 +25,7 @@ import MissionStepByStep from '../containers/teacher/Mission/MissionSBSContainer
 import MissionDetailScreen from '../containers/teacher/Mission/MissionDetailContainer';
 import MissionPlayWebView from '../components/Teacher/MissionDetail/MissonPlayWebView';
 import MissonTestPlayWebView from '../components/Teacher/MissionDetail/MissonTestPlayWebView';
-
+import MissionStatisticsScreen from '../components/Teacher/MissionStatistics/MissionStatisticsScreen';
 const TeacherStack = createStackNavigator({
   TabMainTeacher: {
     screen: TabMainTeacher,
@@ -125,6 +125,10 @@ const TeacherStack = createStackNavigator({
   },
   MissonTestPlayWebView: {
     screen: MissonTestPlayWebView,
+    navigationOptions: { header: null, gesturesEnabled: true },
+  },
+  MissionStatisticsScreen: {
+    screen: MissionStatisticsScreen,
     navigationOptions: { header: null, gesturesEnabled: true },
   }
 }, transition);
