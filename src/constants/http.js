482cdf1fd4763d96899eaf23e5df4f919f56723d
sonmin <sonbv@edmicro.vn>
2020-12-17 15:34:53 +0700

    update link store

--- src/constants/http.js
+++ src/constants/http.js
@@ -1,5 +1,5 @@
 export const GOOGLE_PLAY_APP = 'https://play.google.com/store/apps/details?id=vn.onluyen.app&hl=vi';
-export const ITUNES_APPLE_APP = 'https://itunes.apple.com/us/app/on-luyen/id1308936267?mt=8';
+export const ITUNES_APPLE_APP = 'https://apps.apple.com/us/app/onluyen-gi%C3%A1o-vi%C3%AAn/id1533222817';
 export const FB_PAGE = 'https://www.facebook.com/Edmicro.vn';
 export const YOUTUBE_PAGE = 'https://www.youtube.com/channel/UCOr7LcK1IvbRE_Q7IsuXAWQ';
 export const ITUNES_APPLE_APP_FB = 'https://apps.apple.com/vn/app/facebook/id284882215';
