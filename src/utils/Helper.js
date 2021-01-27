b8f440f982662ed4acfe0f930d2ff9eaadb40ffd
sonmin <sonbv@edmicro.vn>
2021-01-27 15:26:04 +0700

    update process

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -44,6 +44,117 @@ export const getSourceAvatarOwl = (source) => {
     }
 }
 
+export const SubjectIds = () => {
+    return [
+        {
+            id: "ANHTIEUHOC",
+            name: "english"
+        },
+        {
+            id: "HOCHAY",
+            name: "math"
+        },
+        {
+            id: "TOANVAO10",
+            name: "math"
+        },
+        {
+            id: "TOAN",
+            name: "math"
+        },
+        {
+            id: "VATLY",
+            name: "physics"
+        },
+        {
+            id: "SINH",
+            name: "biology"
+        },
+        {
+            id: "HOAHOC",
+            name: "chemistry"
+        },
+        {
+            id: "TIENGANH",
+            name: "english"
+        },
+        {
+            id: "LICHSU",
+            name: "history"
+        },
+        {
+            id: "DIALY",
+            name: "geography"
+        },
+        {
+            id: "GDCD",
+            name: "civiceducation"
+        },
+        {
+            id: "ANHTHCS",
+            name: "english"
+        },
+        {
+            id: "ANHTHPT",
+            name: "english"
+        },
+        {
+            id: "ANHTHPT2",
+            name: "english"
+        },
+        {
+            id: "TIENGANHTHPT3",
+            name: "english10"
+        },
+        {
+            id: "ANHGENERAL",
+            name: "english"
+        },
+        {
+            id: "NGUVANTHCS",
+            name: "literature"
+        },
+        {
+            id: "NGUVAN",
+            name: "literature"
+        },
+        {
+            id: "TOANTHCS",
+            name: "math"
+        },
+        {
+            id: "VATLITHCS",
+            name: "physics"
+        },
+        {
+            id: "LICHSUTHCS",
+            name: "history"
+        },
+        {
+            id: "DIALITHCS",
+            name: "geography"
+        },
+        {
+            id: "GDCDTHCS",
+            name: "civiceducation"
+        },
+        {
+            id: "TINHOCTHPT",
+            name: "tinhoc"
+        },
+    ]
+}
+
+
+export const hasManySubjects = (subjectId) => {
+    let hasManySubject = false;
+    
+    let data = SUBJECTS.find(item=>item.id == subjectId);
+    if(data){
+
+    }
+}
+
 
 
 export const delay = (timeout = 350) => {
