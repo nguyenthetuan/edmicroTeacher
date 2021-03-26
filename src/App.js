283cdb39209926efb9d2f6c37eeb891a3a41c3ab
Edmicro#3 <edmicro3@Mac-mini-cua-Edmicro3.local>
2021-03-26 10:20:43 +0700

    add update require version new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, {Component} from 'react';
-import {Text, TextInput} from 'react-native';
-import {enableScreens} from 'react-native-screens';
-import {Provider} from 'react-redux';
+import React, { Component } from 'react';
+import { Text, TextInput } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+import { Provider } from 'react-redux';
 import codePush from "react-native-code-push";
 import store from './store/index';
 import sagaMiddleware from './middleware/sagaMiddleWare';
 import rootSaga from './sagas/rootSaga';
-import Mixpanel, {MixpanelInstance} from 'react-native-mixpanel';
+import Mixpanel, { MixpanelInstance } from 'react-native-mixpanel';
 import Common from './utils/Common';
 import dataHelper from './utils/dataHelper';
 import jwtDecode from 'jwt-decode';
 import apiUser from './services/apiUserHelper';
 import RootAppNavigator from './navigations/RootNavigator';
-import {StatusBar, Platform} from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import ConfigNotification from './utils/ConfigNotification';
+import UpdateApp from './components/common/UpdateApp';
+import firestore from '@react-native-firebase/firestore';
+import { APP_VERSION_CODE } from './constants/buildConfig';
+import AsyncStorage from '@react-native-community/async-storage';
 
 let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
@@ -24,6 +28,7 @@ class App extends Component {
     super(props);
     this.initMixpanel();
     this.getActiveRoute = this.getActiveRoute.bind(this);
+    this.version = '';
   }
 
   async initMixpanel() {
@@ -33,14 +38,15 @@ class App extends Component {
 
   componentDidMount() {
     Mixpanel.sharedInstanceWithToken(Common.MixpanelToken);
+    this.updataVersionApp();
     console.disableYellowBox = true;
     this.configureTextProps();
-    dataHelper.getToken().then(({token}) => {
-      const {userId, idMixpanel} = jwtDecode(token);
+    dataHelper.getToken().then(({ token }) => {
+      const { userId, idMixpanel } = jwtDecode(token);
       if (!idMixpanel || idMixpanel !== Common.MixpanelToken) {
         Mixpanel.createAlias(userId);
         apiUser
-          .updateMixpanelId({token, mixpanelId: Common.MixpanelToken})
+          .updateMixpanelId({ token, mixpanelId: Common.MixpanelToken })
           .then((data) => {
             if (!!data) {
               Mixpanel.set(jwtDecode(data.access_token));
@@ -52,6 +58,64 @@ class App extends Component {
           });
       }
     });
+  }
+
+  updataVersionApp = async () => {
+    try {
+      // get versionCode on device
+      // version old
+      let versionCode = APP_VERSION_CODE;
+      versionCode = parseInt(versionCode);
+      //get versionCode on firebase
+      const usersCollection = await firestore()
+        .collection('version')
+        .doc('versionAppSchool')
+        .get();
+      const UpdateAppOnluyen = await AsyncStorage.getItem('UpdateAppOnluyen');
+      if (usersCollection._data) {
+        let {
+          versionAndroid,
+          versionIos,
+          isLockUpdate,
+          isLockAndroid,
+          isLockIOS,
+          isRequiredUpdate
+        } = usersCollection._data;
+        if (!isLockUpdate) {
+          if (!isLockAndroid && Platform.OS == 'android') {
+            //version news
+            this.version = parseInt(versionAndroid);
+            if (
+              UpdateAppOnluyen != 'NoNeverShowUpdate' &&
+              this.version > versionCode
+            ) {
+              // nếu người dùng không tắt chức năng hiển thị
+              // và phiên bản hiện tại thấp phiên bản mới trên appStore hay CHPlay
+              // và mới chỉ áp dụng cho android
+              setTimeout(() => {
+                this.refUpdateApp._handleShow(this.version, isRequiredUpdate);
+              }, 10000);
+            }
+          } else if (!isLockIOS && Platform.OS == 'ios') {
+            //version news
+            this.version = parseInt(versionIos);
+            if (
+              UpdateAppOnluyen != 'NoNeverShowUpdate' &&
+              this.version > versionCode
+            ) {
+              // nếu người dùng không tắt chức năng hiển thị
+              // và phiên bản hiện tại thấp phiên bản mới trên appStore hay CHPlay
+              // và mới chỉ áp dụng cho android
+              setTimeout(() => {
+                this.refUpdateApp._handleShow(this.version, isRequiredUpdate);
+              }, 10000);
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   configureTextProps = () => {
@@ -76,6 +140,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <ConfigNotification />
+        <UpdateApp ref={ref => this.refUpdateApp = ref} version={this.version} />
         <RootAppNavigator
           onNavigationStateChange={(prevState, currentState, action) => {
             const currentScreen = this.getActiveRoute(currentState);
