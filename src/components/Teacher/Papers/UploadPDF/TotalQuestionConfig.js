39c9e612c176be0bb714f7cfdb09c7964ef4c64a
vudv@edmicro.vn <vutd@edmicro.vn>
2021-04-23 09:56:54 +0700

    fix bug

--- src/components/Teacher/Papers/UploadPDF/TotalQuestionConfig.js
+++ src/components/Teacher/Papers/UploadPDF/TotalQuestionConfig.js
@@ -71,7 +71,7 @@ export default function TotalQuestionConfig(props) {
                 data={props.questionList}
                 keyExtractor={(item, index) => index.toString()}
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ width: '100%', height: flatlistHeight }}
+                contentContainerStyle={{ width: '100%', height: flatlistHeight + 5 }}
                 onLayout={(e) => {
                     console.log(e.nativeEvent.layout);
                 }}
